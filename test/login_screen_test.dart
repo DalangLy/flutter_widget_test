fcbc41b7839db8c7fb875b65a8d52559230fb677
Dalang <dalangly@gmail.com>
2024-05-17 15:47:44 +0700

    separate widget test

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -10,13 +10,48 @@ void main() {
     );
   }
 
+  testWidgets('Find Username TextField with label of Username', (WidgetTester tester) async {
+    await tester.pumpWidget( buildLoginScreen());
+    // find username field
+    final findUsernameTextFormField = find.byWidgetPredicate((widget) {
+      return widget is TextField
+          && !widget.obscureText
+          && widget.decoration?.labelText == 'Username';
+    });
+    expect(findUsernameTextFormField, findsOneWidget);
+  });
+
+  testWidgets("Find Password TextField with label of Password", (WidgetTester tester) async {
+    await tester.pumpWidget( buildLoginScreen());
+    // find password field
+    final findPasswordTextFormField = find.byWidgetPredicate((widget) {
+      return widget is TextField
+          && widget.obscureText
+          && widget.decoration?.labelText == 'Password';
+    });
+    expect(findPasswordTextFormField, findsOneWidget);
+  });
+
+  testWidgets("Find Login Button with label of Login", (WidgetTester tester) async {
+    await tester.pumpWidget( buildLoginScreen());
+    // find login button
+    final findLoginButton = find.widgetWithText(
+        ElevatedButton,
+        "Login"
+    );
+    expect(findLoginButton, findsOneWidget);
+  });
+
   testWidgets("Find Password Visibility Icon", (tester) async {
     await tester.pumpWidget( buildLoginScreen());
 
     final findPasswordVisibilityIcon = find.byIcon(Icons.visibility_off);
     expect(findPasswordVisibilityIcon, findsOneWidget);
+  },);
 
-
+  testWidgets("Toggle Password Visibility", (WidgetTester tester) async {
+    await tester.pumpWidget( buildLoginScreen());
+    final findPasswordVisibilityIcon = find.byIcon(Icons.visibility_off);
     // tab on password visibility icon and it change to password not visible
     await tester.tap(findPasswordVisibilityIcon);
     await tester.pump();
@@ -29,7 +64,7 @@ void main() {
           && widget.decoration?.labelText == 'Password';
     });
     expect(findPasswordTextFormField, findsOneWidget);
-  },);
+  });
 
   testWidgets("Tab Login Button and show alert dialog with loading", (WidgetTester tester) async {
     await tester.pumpWidget( buildLoginScreen());
@@ -44,30 +79,4 @@ void main() {
     final findAlertDialog = find.byType(AlertDialog);
     expect(findAlertDialog, findsOneWidget);
   },);
-
-  testWidgets('Find All Essential Widgets in login screen', (WidgetTester tester) async {
-    await tester.pumpWidget( buildLoginScreen());
-    // find username field
-    final findUsernameTextFormField = find.byWidgetPredicate((widget) {
-      return widget is TextField
-          && !widget.obscureText
-          && widget.decoration?.labelText == 'Username';
-    });
-    expect(findUsernameTextFormField, findsOneWidget);
-
-    // find password field
-    final findPasswordTextFormField = find.byWidgetPredicate((widget) {
-      return widget is TextField
-          && widget.obscureText
-          && widget.decoration?.labelText == 'Password';
-    });
-    expect(findPasswordTextFormField, findsOneWidget);
-
-    // find login button
-    final findLoginButton = find.widgetWithText(
-      ElevatedButton,
-      "Login"
-    );
-    expect(findLoginButton, findsOneWidget);
-  });
 }
